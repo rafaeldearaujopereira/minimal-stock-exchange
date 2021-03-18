547a9f685be0410fa8c0928c432ff81d1ff03877
rafaeldearaujopereira <rafael.a.pereira@gmail.com>
2021-03-17 21:01:34 -0300

    basic entities

---
+++ MinimalStockExchange/src/br/com/agorainvestimentos/exchange/model/Client.java
@@ -0,0 +1,15 @@
+package br.com.agorainvestimentos.exchange.model;
+
+public class Client {
+
+	private String code;
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+}
---
+++ MinimalStockExchange/src/br/com/agorainvestimentos/exchange/model/Instrument.java
@@ -0,0 +1,15 @@
+package br.com.agorainvestimentos.exchange.model;
+
+public class Instrument {
+
+	private String code;
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+}
