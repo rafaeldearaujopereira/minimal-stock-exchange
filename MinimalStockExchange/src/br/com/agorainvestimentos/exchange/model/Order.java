d360f2e9c39385dba35cfcc77a70df3450b2c4ff
rafaeldearaujopereira <rafael.a.pereira@gmail.com>
2021-03-19 23:43:54 -0300

    more attributes

--- MinimalStockExchange/src/br/com/agorainvestimentos/exchange/model/Order.java
+++ MinimalStockExchange/src/br/com/agorainvestimentos/exchange/model/Order.java
@@ -1,9 +1,37 @@
 package br.com.agorainvestimentos.exchange.model;
 
-import java.math.BigInteger;
+import java.math.BigDecimal;
 
 public class Order {
 
 	private Long id;
-	
+
+	private Instrument instrument;
+
+	private BigDecimal price;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Instrument getInstrument() {
+		return instrument;
+	}
+
+	public void setInstrument(Instrument instrument) {
+		this.instrument = instrument;
+	}
+
+	public BigDecimal getPrice() {
+		return price;
+	}
+
+	public void setPrice(BigDecimal price) {
+		this.price = price;
+	}
+
 }
