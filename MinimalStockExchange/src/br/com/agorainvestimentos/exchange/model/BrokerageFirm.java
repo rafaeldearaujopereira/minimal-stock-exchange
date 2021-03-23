6ce3c63601c0974f36af7a93baa988caf0724fc7
rafaeldearaujopereira <rafael.a.pereira@gmail.com>
2021-03-22 22:29:47 -0300

    adding attributes to entities.

--- MinimalStockExchange/src/br/com/agorainvestimentos/exchange/model/BrokerageFirm.java
+++ MinimalStockExchange/src/br/com/agorainvestimentos/exchange/model/BrokerageFirm.java
@@ -2,7 +2,17 @@ package br.com.agorainvestimentos.exchange.model;
 
 public class BrokerageFirm {
 
+	private String code;
+
 	private String name;
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
 
 	public String getName() {
 		return name;
