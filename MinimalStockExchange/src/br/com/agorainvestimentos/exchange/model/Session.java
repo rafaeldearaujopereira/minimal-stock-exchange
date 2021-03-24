fac38d2c3f3dcb4c27c28e08f35087ca8beeaf0b
rafaeldearaujopereira <rafael.a.pereira@gmail.com>
2021-03-23 22:30:10 -0300

    adding session entity (for FIX sessions).

---
+++ MinimalStockExchange/src/br/com/agorainvestimentos/exchange/model/Session.java
@@ -0,0 +1,15 @@
+package br.com.agorainvestimentos.exchange.model;
+
+public class Session {
+
+	private BrokerageFirm brokerage;
+
+	public BrokerageFirm getBrokerage() {
+		return brokerage;
+	}
+
+	public void setBrokerage(BrokerageFirm brokerage) {
+		this.brokerage = brokerage;
+	}
+
+}
