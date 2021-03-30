e41af0926d913da3952d349f5d3e97c189953a1f
rafaeldearaujopereira <rafael.a.pereira@gmail.com>
2021-03-29 21:51:42 -0300

    Trade entity.

--- MinimalStockExchange/src/br/com/agorainvestimentos/exchange/model/Trade.java
+++ MinimalStockExchange/src/br/com/agorainvestimentos/exchange/model/Trade.java
@@ -1,5 +1,49 @@
 package br.com.agorainvestimentos.exchange.model;
 
+import java.math.BigDecimal;
+
 public class Trade {
+	
+	private Order buyOrder;
+	
+	private Order sellOrder;
+	
+	private Long quantity;
+	
+	private BigDecimal volume;
+
+	public Order getBuyOrder() {
+		return buyOrder;
+	}
+
+	public void setBuyOrder(Order buyOrder) {
+		this.buyOrder = buyOrder;
+	}
+
+	public Order getSellOrder() {
+		return sellOrder;
+	}
+
+	public void setSellOrder(Order sellOrder) {
+		this.sellOrder = sellOrder;
+	}
+
+	public Long getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(Long quantity) {
+		this.quantity = quantity;
+	}
+
+	public BigDecimal getVolume() {
+		return volume;
+	}
+
+	public void setVolume(BigDecimal volume) {
+		this.volume = volume;
+	}
+	
+	
 
 }
